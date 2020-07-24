ef8edaad3f382eab106bc8fdc84c5179a6c479ff
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-07-24 23:30:06 +0900

    Add ウェルカムメッセージのenvを移行

--- src/client/guildMemberAdd.ts
+++ src/client/guildMemberAdd.ts
@@ -1,5 +1,6 @@
 import * as Discord from 'discord.js'
 import ThrowEnv from 'throw-env'
+import Settings from 'const-settings'
 
 /**
  * 新規メンバーに方針チャンネルを見るよう催促するメッセージを送信する
@@ -11,9 +12,10 @@ export const GuildMemberAdd = (client: Discord.Client, member: Discord.GuildMemb
   if (member.guild.id !== ThrowEnv('CLAN_SERVER_ID')) return
 
   // ウェルカムチャンネルに催促メッセージを送信
-  const channel = client.channels.cache.get(ThrowEnv('WELCOME_CHANNEL_ID')) as Discord.TextChannel
-  const firstLine = `<@!${member.user?.id}> まずは <#${ThrowEnv('POLICY_CHANNEL_ID')}> を確認しなさい！`
-  const secondLine = `ちゃんと <#${ThrowEnv('SELF_INTRODUCTION_ID')}> も書くことね`
+  const channel = client.channels.cache.get(Settings.WELCOME_CHANNEL.CHAT_ID) as Discord.TextChannel
+
+  const firstLine = `<@!${member.user?.id}> まずは <#${Settings.WELCOME_CHANNEL.POLICY_ID}> を確認しなさい！`
+  const secondLine = `ちゃんと <#${Settings.WELCOME_CHANNEL.INTRODUCTION_ID}> も書くことね`
   channel?.send(`${firstLine}\n${secondLine}`)
 
   console.log(`I’m ${member.user?.username}, a new member.`)
