6d7430266b9075df52bf5c435aa23fbf70164f9b
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-07-24 23:23:41 +0900

    Add 起動時のenvを移行

--- dist/client/ready.js
+++ dist/client/ready.js
@@ -4,11 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 exports.Ready = void 0;
-var throw_env_1 = __importDefault(require("throw-env"));
 var const_settings_1 = __importDefault(require("const-settings"));
 exports.Ready = function (client) {
     var _a;
-    var channel = client.channels.cache.get(throw_env_1["default"]('NOTIFY_CHANNEL_ID'));
-    channel === null || channel === void 0 ? void 0 : channel.send(const_settings_1["default"].STARTUP_MESSAGE);
+    var channel = client.channels.cache.get(const_settings_1["default"].STARTUP.CHANNEL_ID);
+    channel === null || channel === void 0 ? void 0 : channel.send(const_settings_1["default"].STARTUP.MESSAGE);
     console.log("Logged in as " + ((_a = client.user) === null || _a === void 0 ? void 0 : _a.username) + "!");
 };
