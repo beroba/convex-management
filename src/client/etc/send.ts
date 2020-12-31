c081d109a0fc09b6c396936e05c8c76735d8238f
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2021-01-01 00:41:59 +0900

    add あつもりでも動くようにしましょう

--- src/client/etc/send.ts
+++ src/client/etc/send.ts
@@ -297,7 +297,7 @@ export const SendEmoji = async (msg: Discord.Message): Promise<Option<string>> =
     return 'Send kmr Emoji'
   }
 
-  if (msg.content === '熱盛') {
+  if (msg.content === '熱盛' || msg.content === 'あつもり') {
     // 焼肉カンパニ！の絵文字を送信
     await msg.channel.send('<:heike:670116923127889924> ')
 
