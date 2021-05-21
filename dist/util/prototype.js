af533d835170c517bc7bd1c04afa67e0446b203a
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2021-05-21 22:46:53 +0900

    rm dist/

--- dist/util/prototype.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-Array.prototype.first = function () {
-    return this[0];
-};
-Array.prototype.last = function () {
-    return this[this.length - 1];
-};
