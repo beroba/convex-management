08775690a6779cc8fd1aa2e21ae8de0bdaf1a347
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2021-08-30 23:43:47 +0900

    fix send

--- src/util/send.ts
+++ src/util/send.ts
@@ -129,20 +129,18 @@ const createRandNumber = (n: number): number => {
  * @return カンカンカンが含まれていたかの結果
  */
 export const GoodMorning = (msg: Discord.Message): Option<string> => {
-  // botのメッセージは実行しない
-  if (msg.member?.user.bot) return
+  const isBot = msg.member?.user.bot
+  if (isBot) return
 
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.MAJOR_THAT_CHANNEL, msg.channel)) return
+  const isMajorThat = util.IsChannel(Settings.MAJOR_THAT_CHANNEL, msg.channel)
+  if (!isMajorThat) return
 
-  // カンカンカンの文字が含まれているか確認
-  if (!msg.content.match(/カンカンカン/)) return
+  const matchKanKanKan = msg.content.match(/カンカンカン/)
+  if (!matchKanKanKan) return
 
-  // カンカンカンのメッセージ
   const message =
     'おはよー！！！カンカンカン！！！起きなさい！！！クラバトよ！！！！すごいクラバトよ！！！！外が明るいわよ！！カンカンカンカンカン！！！！！おはよ！！カンカンカン！！！見て見て！！！！外明るいの！！！外！！！！見て！！カンカンカンカンカン！！凸しなさい！！早く凸して！！カンカン！ぶっ殺すわよ！！！！！！！！！！:heavy_check_mark:'
 
-  // メッセージ送信先のチャンネルを取得
   const channel = util.GetTextChannel(msg.channel.id)
   channel.send(message)
 
@@ -155,16 +153,12 @@ export const GoodMorning = (msg: Discord.Message): Option<string> => {
  * @return 画像を送信したかの結果
  */
 export const YabaiImage = (msg: Discord.Message): Option<string> => {
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)) return
+  const isNetaThat = util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)
+  if (!isNetaThat) return
 
-  // 文字にやばいが含まれているか確認
-  const match = msg.content.replace(/やばい|ヤバい/g, 'ヤバイ').match(/ヤバイ/)
+  const matchYabai = msg.content.replace(/やばい|ヤバい/g, 'ヤバイ').match(/ヤバイ/)
+  if (!matchYabai) return
 
-  // 含まれていない場合は終了
-  if (!match) return
-
-  // ヤバイわよ！の画像を送信
   msg.channel.send({files: [Settings.URL.YABAIWAYO]})
 
   return 'Send Yabai Image'
@@ -176,17 +170,13 @@ export const YabaiImage = (msg: Discord.Message): Option<string> => {
  * @return 画像を送信したかの結果
  */
 export const ShinyTmoImage = (msg: Discord.Message): Option<string> => {
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)) return
+  const isNetaThat = util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)
+  if (!isNetaThat) return
 
-  // 文字がシャイニートモか確認
-  if (msg.content !== 'シャイニートモ') return
+  const matchShinyTmo = msg.content === 'シャイニートモ'
+  if (!matchShinyTmo) return
 
-  // シャイニートモの画像を送信
   msg.channel.send({files: [Settings.URL.SHINYTMO]})
-
-  // 元のメッセージは削除
-  setTimeout(() => msg.delete(), 100)
 
   return 'Send ShinyTmo Image'
 }
@@ -197,13 +187,12 @@ export const ShinyTmoImage = (msg: Discord.Message): Option<string> => {
  * @return 画像を送信したかの結果
  */
 export const TasuketeImage = (msg: Discord.Message): Option<string> => {
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)) return
+  const isNetaThat = util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)
+  if (!isNetaThat) return
 
-  // 文字が助けて！か確認
-  if (!/^助けて(!|！)$/.test(msg.content)) return
+  const matchTasukete = /^助けて(!|！)$/.test(msg.content)
+  if (!matchTasukete) return
 
-  // 助けて！の画像を送信
   msg.channel.send({files: [Settings.URL.TASUKETE]})
 
   return 'Send Tasukete Image'
@@ -215,35 +204,32 @@ export const TasuketeImage = (msg: Discord.Message): Option<string> => {
  * @return 画像を送信したかの結果
  */
 export const KusaGacha = async (msg: Discord.Message): Promise<Option<string>> => {
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)) return
+  const isNetaThat = util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)
+  if (!isNetaThat) return
 
-  // 文字が草か確認
-  if (!/^草\s?\d*$/.test(msg.content)) return
+  const matchKusa = /^草\s?\d*$/.test(msg.content)
+  if (!matchKusa) return
 
   // ガチャの内容を作成
   const items: string[] = Settings.KUSA.map((v: {NAME: string; RATIO: string}) => Array(v.RATIO).fill(v.NAME)).flat()
 
-  // 抽選する回数を取得
-  const c = msg.content.replace(/[^1-5]/g, '').to_n()
-  if (c) {
-    util.Range(c > 5 ? 5 : c).forEach(_ => {
-      // リストの数に応じて乱数を作る
-      const rand = createRandNumber(items.length)
-      // 草の画像を送信
-      msg.reply({
-        content: `${items[rand]}:heavy_check_mark:`,
-        files: [`./assets/kusa/${items[rand]}.png`],
-      })
-    })
-  } else {
-    // リストの数に応じて乱数を作る
+  /**
+   * 草をランダムで送信する
+   */
+  const sendKusa = () => {
     const rand = createRandNumber(items.length)
-    // 草の画像を送信
     msg.reply({
       content: `${items[rand]}:heavy_check_mark:`,
       files: [`./assets/kusa/${items[rand]}.png`],
     })
+  }
+
+  // 抽選する回数を取得
+  const c = msg.content.replace(/[^1-5]/g, '').to_n()
+  if (c) {
+    util.Range(c > 5 ? 5 : c).forEach(_ => sendKusa())
+  } else {
+    sendKusa()
   }
 
   return 'Send Kusa Gacha'
@@ -255,26 +241,23 @@ export const KusaGacha = async (msg: Discord.Message): Promise<Option<string>> =
  * @return ツイートを送信したかの結果
  */
 export const SendUsoOre = async (msg: Discord.Message): Promise<Option<string>> => {
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)) return
+  const isNetaThat = util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)
+  if (!isNetaThat) return
 
-  // 文字が俺嘘か確認
-  if (!/^(俺嘘|嘘俺)\s?([1-5]|読み?)*$/.test(msg.content)) return
+  const matchUsoOre = /^(嘘俺|俺嘘)\s?([1-5]|読み?)*$/.test(msg.content)
+  if (!matchUsoOre) return
 
-  // #さとりんご名言ツイートからTweet一覧を取得する
   const list = await fetchTweetList()
   if (!list) return
 
-  // 読み?がある場合は読み上げをする
-  if (/読み?/.test(msg.content)) {
-    // リストの数に応じて乱数を作る
+  const matchYomi = /読み?/.test(msg.content)
+  if (matchYomi) {
     const rand = createRandNumber(list.length)
     msg.reply(list.splice(rand, 1).first())
 
     return 'Send UsoOre'
   }
 
-  // Tweetリストから選別して送信
   sendTweetLottery(list, msg)
 
   return 'Send UsoOre'
@@ -286,20 +269,17 @@ export const SendUsoOre = async (msg: Discord.Message): Promise<Option<string>> 
  * @return ツイートを送信したかの結果
  */
 export const SendAguhiyori = async (msg: Discord.Message): Promise<Option<string>> => {
-  // 指定のチャンネル以外では実行されない用にする
-  if (!util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)) return
+  const isNetaThat = util.IsChannel(Settings.NETA_THAT_CHANNEL, msg.channel)
+  if (!isNetaThat) return
 
-  // 文字が俺嘘か確認
-  if (!/^アザラシ\s?([1-5]|読み?)*$/.test(msg.content)) return
+  const matchAzarashi = /^アザラシ\s?([1-5]|読み?)*$/.test(msg.content)
+  if (!matchAzarashi) return
 
-  // #さとりんご名言ツイートからTweet一覧を取得する
   let list = await fetchTweetList()
   if (!list) return
 
   // アザラシシーパラダイスのツイートだけ選別
   list = list.filter(m => /aguhiyori/.test(m))
-
-  // Tweetリストから選別して送信
   sendTweetLottery(list, msg)
 
   return 'Send Aguhiyori'
@@ -309,34 +289,32 @@ export const SendAguhiyori = async (msg: Discord.Message): Promise<Option<string
  * #さとりんご名言ツイートからTweet一覧を取得する
  */
 const fetchTweetList = async () => {
-  // さとりんご名言ツイートのメッセージを取得
   const channel = util.GetTextChannel(Settings.CHANNEL_ID.OREUSO)
   const msgs: Discord.Message[] = []
 
-  // 取得するメッセージの上限を設定
+  // 最終的に取得するメッセージの上限を設定
   const limit = 500
   const rounds = limit / 100 + (limit % 100 ? 1 : 0)
 
-  // 取得した最後のメッセージを保存
+  // 取得した最後のメッセージIDを保存
   let last_id = ''
   for (let i = 0; i < rounds; i++) {
-    // オプションの設定
+    // 上限が100件なので100に設定
     const options: Discord.ChannelLogsQueryOptions = {limit: 100}
     if (last_id.length > 0) {
       options.before = last_id
     }
 
-    // メッセージを取得
     const messages = (await channel.messages.fetch(options)).map(m => m)
     msgs.push(...messages)
 
-    // 最後のメッセージを更新
+    // 最後のメッセージIDを更新
     last_id = messages.last().id
-    // 最後のメッセージが特定のメッセージなら終了
+
+    // 最後のメッセージが嘘俺の最後のメッセージなら強制終了
     if (last_id === Settings.UsoOreLast) break
   }
 
-  // ツイートの一覧を取得
   return msgs
     .map(m => m.embeds)
     .reduce((pre, current) => {
@@ -358,37 +336,33 @@ const sendTweetLottery = (list: string[], msg: Discord.Message) => {
   const c = msg.content.replace(/[^1-5]/g, '').to_n()
   if (c) {
     util.Range(c > 5 ? 5 : c).forEach(c => {
-      // リストの数に応じて乱数を作る
       const rand = createRandNumber(list.length)
       msg.reply(`${c + 1}:heavy_check_mark:\n${list.splice(rand, 1).first().split('\n').last()}`)
     })
   } else {
-    // リストの数に応じて乱数を作る
     const rand = createRandNumber(list.length)
     msg.reply(list.splice(rand, 1).first().split('\n').last())
   }
 }
 
 /**
- #肉に画像が送信された際に肉の絵文字を付ける
+ * #肉に画像が送信された際に肉の絵文字を付ける
  * @param msg DiscordからのMessage
  * @return リアクションしたかの結果
  */
 export const NikuPicture = (msg: Discord.Message): Option<string> => {
-  // #肉でなければ終了
-  if (msg.channel.id !== Settings.CHANNEL_ID.NIKU) return
+  const isNiku = msg.channel.id === Settings.CHANNEL_ID.NIKU
+  if (!isNiku) return
 
-  // メッセージに画像があるか確認する
-  const url = msg.attachments.map(a => a.url).first()
-  if (!url) return
+  const isImage = msg.attachments.map(a => a.url).first()
+  if (!isImage) return
 
-  // 肉の絵文字をつける
   msg.react(Settings.EMOJI_ID.NIKU)
 
   // 飯テロリストのロールを付与する
   msg.member?.roles.add(Settings.ROLE_ID.MESHI_TERO)
 
-  return 'React Niku Channel'
+  return 'React Niku And Meshitero Role'
 }
 
 /**
@@ -397,10 +371,11 @@ export const NikuPicture = (msg: Discord.Message): Option<string> => {
  * @return ロールを付与したかの結果
  */
 export const AddSeihekiRole = (msg: Discord.Message): Option<string> => {
-  // #性癖調査18禁 #性癖調査よろず18禁でなければ終了
-  if (msg.channel.id !== Settings.CHANNEL_ID.SEIHEKI && msg.channel.id !== Settings.CHANNEL_ID.SEIHEKI_YOROZU) return
+  const notIsSeheki =
+    msg.channel.id !== Settings.CHANNEL_ID.SEIHEKI && msg.channel.id !== Settings.CHANNEL_ID.SEIHEKI_YOROZU
+  if (notIsSeheki) return
 
-  // 飯テロリストのロールを付与する
+  // 性癖調査をしろのロールを付与する
   msg.member?.roles.add(Settings.ROLE_ID.SEIHEKI)
 
   return 'Add Seiheki Role'
